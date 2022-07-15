039261b7e9bc3a3a36458bcfe42596b17a38da46
LeeYongwoo-kor <newlyo1205@naver.com>
2022-07-15 21:05:23 +0900

    Add ES2022(Class Field Declaration)

--- ES2022.js
+++ ES2022.js
@@ -19,3 +19,34 @@ const err = new Error("Can't save comment", { cause: "Not allowed." });
 
 console.log(err.message);
 console.log(err.cause);
+
+// Class Field Declaration
+class SampleClass {
+  /*
+    instead of:
+    constructor() { this.publicID = 42; }
+  */
+  publicID = 42; // public field
+
+  /*
+    instead of:
+    static get staticPublicField() { return -1 }
+  */
+  static staticPublicField = -1;
+
+  // static private field
+  static #staticPrivateField = "private";
+
+  //private methods
+  #privateMethod() {}
+
+  // static block
+  static {
+    // executed when the class is created
+  }
+}
+
+const sample = new SampleClass();
+console.log(sample.publicID);
+console.log(SampleClass.staticPublicField);
+console.log(SampleClass.staticPrivateField); // undefined
